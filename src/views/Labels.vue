1ce3c3329925f8aeb3118bcf4f91436c2de4ff6c
summer <1065300945@qq.com>
2021-01-14 23:51:36 +0800

    全局组件<Nav/>

---
+++ src/views/Labels.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Labels.vue
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+  name: 'Labels',
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Statistics.vue
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+
+
+export default {
+  name: 'Statistics',
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
