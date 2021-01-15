9967c11f20986e65aeccd719b73d3322fc00d987
summer <1065300945@qq.com>
2021-01-15 23:35:05 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,38 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
-import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-    {
-        path: '/',
-        redirect: 'money'
-    },
-    {
-        path: '/money',
-        component: Money
-    },
-    {
-        path: '/labels',
-        component: Labels
-    },
-    {
-        path: '/statistics',
-        component: Statistics
-    }
+  {
+    path: '/',
+    redirect: 'money'
+  },
+  {
+    path: '/money',
+    component: Money
+  },
+  {
+    path: '/labels',
+    component: Labels
+  },
+  {
+    path: '/statistics',
+    component: Statistics
+  },
+  {
+    // 会匹配所有路径
+    path: '*',
+    component: NotFound
+  }
 ];
 
 const router = new VueRouter({
-    routes
+  routes
 });
 
 export default router;
