789681988ffe827d89ee9d3b6094c99b53674c20
summer <1065300945@qq.com>
2021-01-15 23:59:10 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,18 +1,64 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 
-
 export default {
   name: 'Money',
-
 };
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex; //使用flex布局
+  flex-direction: column; //竖着布局
+  height: 100vh; //高度就是100%
+}
+
+.content {
+  border: 1px solid blue;
+  overflow: auto; //滚动条
+  flex-grow: 1; //让蓝色的区域尽量占满
+}
 
 </style>
