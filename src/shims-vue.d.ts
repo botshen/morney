4a9d867e4229c935f3f501935ca91a5c4c808476
summer <1065300945@qq.com>
2021-01-17 23:21:09 +0800

    创建 <Icon />

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,9 @@
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import Vue from 'vue';
+  export default Vue;
+}
+//引入svg提示解决代码
+declare module '*.svg' {
+  const content: string;
+  export default content;
 }
