885c867ba066c547b909c67813dc009bf72cd6ab
summer <1065300945@qq.com>
2021-01-29 00:01:39 +0800

    收集四个组件的数据到 record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -26,10 +26,12 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
-  output = '0';
+  @Prop() readonly value!: number;
+  output = this.value.toString();
 
   inputContent(event: MouseEvent) {
     const button = (event.target as HTMLButtonElement);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const input = button.textContent!;
     if (this.output.length === 16) {return;}
     if (this.output === '0') {
