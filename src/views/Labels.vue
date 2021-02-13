4b3cd7b0f501eb48ce12f3448fc4c0fa8cc851df
summer <1065300945@qq.com>
2021-02-13 20:28:12 +0800

    label 标签页的 html 和 css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,26 @@
 <template>
   <Layout>
-    Labels.vue
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
   </Layout>
 </template>
 
@@ -9,3 +29,40 @@ export default {
   name: 'Labels',
 };
 </script>
+
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+
+    svg {
+      height: 18px;
+      width: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
+</style>
