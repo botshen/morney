53eb935bb1c728d8a99bd4f382b9e40a65f750ed
summer <1065300945@qq.com>
2021-03-01 20:23:43 +0800

    。。。

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -21,4 +21,6 @@ type TagListModel = {
 interface Window {
   tagList: Tag[];
   createTag: (name: string) => void;
+  removeTag: (id: string) => boolean;
+  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
 }
