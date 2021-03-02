6363aa38ef508213f5234b90146d1912cd5e58ba
summer <1065300945@qq.com>
2021-03-02 21:32:36 +0800

    封装 record 的操作

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,11 +15,7 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
-
-
-const recordList = recordListModel.fetch();
+import {Component} from 'vue-property-decorator';
 
 
 @Component({
@@ -27,7 +23,7 @@ const recordList = recordListModel.fetch();
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList = window.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -41,12 +37,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    recordListModel.create(this.record);
-  }
-
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
+    window.createRecord(this.record);
   }
 }
 </script>
