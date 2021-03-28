ee4c451ab860c378cb6bcb6f273c353932c9eddd
botshen <363088847@qq.com>
2021-03-28 12:04:25 +0800

    抽离 Tabs.vue，使用在 Statistics.vue 里面

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value==='-'&&'selected'" @click="selectType('-')">支出</li>
-      <li :class="value==='+'&&'selected'" @click="selectType('+')">收入</li>
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value==='-'}"
+          @click="selectType('-')">支出
+      </li>
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value==='+'}"
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
-  @Prop() readonly value!: string;
+  @Prop(String) readonly value!: string;
+  @Prop(String) classPrefix?: string;
 
   selectType(type: string) {
     if (type !== '-' && type !== '+') {
@@ -26,11 +31,10 @@ export default class Types extends Vue {
 
 <style lang="scss" scoped>
 .types {
-  background: #c4c4c4;
+  background: #C4C4C4;
   display: flex;
   text-align: center;
   font-size: 24px;
-
   > li {
     width: 50%;
     height: 64px;
@@ -38,7 +42,6 @@ export default class Types extends Vue {
     justify-content: center;
     align-items: center;
     position: relative;
-
     &.selected::after {
       content: '';
       position: absolute;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,30 +1,47 @@
 <template>
   <Layout>
-    <Types class="x" :value.sync="yyy"/>
+    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <div>
+      type: {{type}}
+      <br/>
+      interval: {{interval}}
+    </div>
   </Layout>
 </template>
+
 <style scoped lang="scss">
-.x ::v-deep li {
+::v-deep .type-tabs-item {
   background: white;
-
   &.selected {
-    background: #c4c4c4;
-
+    background: #C4C4C4;
     &::after {
       display: none;
     }
   }
 }
 </style>
+
 <script lang="ts">
 import Types from '@/components/Money/Types.vue';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
 
 @Component({
-  components: {Types},
+  components: {Tabs, Types},
 })
 export default class Statistics extends Vue {
-  yyy = '-';
-};
+  type = '-';
+  interval = 'day';
+  intervalList = [
+    {text: '按天', value: 'day'},
+    {text: '按周', value: 'week'},
+    {text: '按月', value: 'month'},
+  ];
+  typeList = [
+    {text: '支出', value: '-'},
+    {text: '收入', value: '+'},
+  ];
+}
 </script>
